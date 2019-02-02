12e2159e5f5f863975d3fac8e2668ec7c03ac12a
subbaparitala <subbaparitala@gmail.com>
2019-02-02 23:17:07 +0530

    user setup done through the devise gem and indentifier added

---
+++ app/controllers/identifiers_controller.rb
@@ -0,0 +1,7 @@
+class IdentifiersController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+
+  end
+end
---
+++ db/migrate/20190202174039_create_identifiers.rb
@@ -0,0 +1,10 @@
+class CreateIdentifiers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :identifiers do |t|
+      t.string :name, null: false
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
